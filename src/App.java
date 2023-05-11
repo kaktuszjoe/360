9b971e5505dfe28d3ae37c1329f331f6185bf591
kaktuszjoe <petigecisanyi@gmail.com>
2023-05-11 13:18:00 +0200

    metodusok

--- src/App.java
+++ src/App.java
@@ -1,5 +1,39 @@
+
+import java.util.Scanner;
+
 public class App {
+    static  double szamol( double a,double b, double c){
+        double V  = a*b*c;
+        return V;
+    }
+
+    public static boolean isNumeric(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        return false;
+    }
+
+    
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("add meg a 3 adatot");
+        String a  = myObj.nextLine();
+        isNumeric(a);
+        String b = myObj.nextLine();
+        isNumeric(b);
+        String c = myObj.nextLine();
+        
+        
+        System.out.println(isNumeric(c));
+       
+
+        
+        
+
+        
+
+        
     }
 }
